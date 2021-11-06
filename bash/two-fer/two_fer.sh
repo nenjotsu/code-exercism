d03619a188163176cbf137fa4bb1aa97b5d648d8
nenjotsu <renenjo.valente@gmail.com>
2021-11-06 22:22:01 +0800

    Affected: update solution in 2-fer

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-function main () {
-  if [ -z "$1" ]; then echo "One for you, one for me."; else echo "One for ${1}, one for me."; fi
-}
-main "$@"
+echo "One for ${1:-you}, one for me."

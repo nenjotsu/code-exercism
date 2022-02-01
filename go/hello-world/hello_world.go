688b8b6470684f7061e9f0c8872056f498471e9d
nenjotsu <renenjo.valente@gmail.com>
2022-02-01 15:30:42 +0800

    add go & rust

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -4,3 +4,14 @@ package greeting
 func HelloWorld() string {
 	return "Hello, World!"
 }
+
+func Gcd(p int, q int) int {
+	if q == 0 {
+		return p
+	}
+
+	r := p % q
+	return Gcd(q, r)
+}
+
+// fmt.Println()

8803036c5efa75fa684b1dcda6f89c98d862e276
nenjotsu <nenjotsu@protonmail.com>
2023-12-24 21:18:52 +0800

    add solution for lasagna_master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,12 +1,39 @@
 package lasagna_master
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, avgTime int) int {
+	if avgTime == 0 {
+		return len(layers) * 2
+	}
+	return len(layers) * avgTime
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (noodles int, sauce float64) {
+	for i := 0; i < len(layers); i++ {
+		if layers[i] == "noodles" {
+			noodles += 50
+		}
+		if layers[i] == "sauce" {
+			sauce += 0.2
+		}
+	}
+	return
+}
 
 // TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsRecipe, myRecipe []string) {
+	myRecipe[len(myRecipe)-1] = friendsRecipe[len(friendsRecipe)-1]
+}
 
 // TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	var scales = make([]float64, len(quantities))
+	for i := 0; i < len(quantities); i++ {
+		scales[i] = quantities[i] * float64(portions) / 2
+	}
+	return scales
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.

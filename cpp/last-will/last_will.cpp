e6dcef3d3afd1f9883b09ab7a19dd0bfc6ae1022
nenjotsu <nenjotsu@protonmail.com>
2024-07-14 19:36:50 +0800

    fix issue in last will cpp

--- cpp/last-will/last_will.cpp
+++ cpp/last-will/last_will.cpp
@@ -45,14 +45,17 @@ namespace garcia {
 // Enter your code below
 namespace estate_executor {
   int assemble_account_number(int secret_modifier) {
-    return (zhang::bank_number_part(secret_modifier) + 
+    return zhang::bank_number_part(secret_modifier) + 
             khan::bank_number_part(secret_modifier) + 
-            garcia::bank_number_part(secret_modifier));
+            garcia::bank_number_part(secret_modifier);
   }
 
   int assemble_code() {
     return (zhang::red::code_fragment() +
             khan::red::code_fragment() +
-            garcia::red::code_fragment());
+            garcia::red::code_fragment()) *
+            (zhang::blue::code_fragment() +
+            khan::blue::code_fragment() +
+            garcia::blue::code_fragment());
   }
 }

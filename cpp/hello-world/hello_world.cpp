02330ab9ab731d17b30f0bcffc4d8714584f8f73
nenjotsu <nenjotsu@protonmail.com>
2024-07-14 15:40:08 +0800

    add hw

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -1,13 +1,18 @@
 #include "hello_world.h"
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 namespace hello_world
 {
-
   string hello()
   {
     return "Hello, World!";
+  }
+
+  int main() {
+    cout << hello_world::hello() << endl;
+    return 0;
   }
 }

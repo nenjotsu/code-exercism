c4c39bb64079a993ae8b5d2ab787a2846a720dea
nenjotsu <nenjotsu@protonmail.com>
2024-07-24 13:36:39 +0800

    add compute tax

---
+++ cpp/exercises/compute_tax.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  double sales = 95000;
+  double stateTax = 0.04;
+  double countyTax = 0.02;
+  double totalTax = stateTax + countyTax;
+  double total = sales - (sales * totalTax);
+  std::cout << total << std::endl;
+  return 0;
+}

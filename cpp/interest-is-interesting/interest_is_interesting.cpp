bdd5bfbe10c8fa49e480f823c0a8a7b134b6fb48
nenjotsu <nenjotsu@protonmail.com>
2024-07-30 17:40:49 +0800

    add solution to cpp interest is interesting

--- cpp/interest-is-interesting/interest_is_interesting.cpp
+++ cpp/interest-is-interesting/interest_is_interesting.cpp
@@ -1,25 +1,30 @@
 // interest_rate returns the interest rate for the provided balance.
 double interest_rate(double balance) {
-    // TODO: Implement the interest_rate function
+    if (balance >= 5000) return 2.475;
+    if (balance >= 1000 && balance < 5000) return 1.621;
+    if (balance >= 0 && balance < 1000) return 0.5;
+    if (balance < 0) return 3.213;
     return 0.0;
 }
 
 // yearly_interest calculates the yearly interest for the provided balance.
 double yearly_interest(double balance) {
-    // TODO: Implement the yearly_interest function
-    return 0.0;
+    return balance * (interest_rate(balance) / 100);
 }
 
 // annual_balance_update calculates the annual balance update, taking into
 // account the interest rate.
 double annual_balance_update(double balance) {
-    // TODO: Implement the annual_balance_update function
-    return 0.0;
+    return yearly_interest(balance) + balance;
 }
 
 // years_until_desired_balance calculates the minimum number of years required
 // to reach the desired balance.
 int years_until_desired_balance(double balance, double target_balance) {
-    // TODO: Implement the years_until_desired_balance function
-    return 0;
+    int years = 0;
+    while (balance < target_balance) {
+        balance = annual_balance_update(balance);
+        years++;  
+    }
+    return years;
 }

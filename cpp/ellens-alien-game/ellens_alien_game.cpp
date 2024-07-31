8593cc420ae607a6872ea697d26ec727e0b10ae8
nenjotsu <renenjo.valente@gmail.com>
2024-07-31 22:08:31 +0800

    add cpp ellens alien solution

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -1,4 +1,37 @@
 namespace targets {
 // TODO: Insert the code for the alien class here
+  class Alien {
+    public:
+      int x_coordinate{0};
+      int y_coordinate{0};
+      Alien(int x, int y){
+        x_coordinate = x;
+        y_coordinate = y;
+      }
+      int get_health() {
+        return health;
+      }
+      bool is_alive() {
+        return health > 0;
+      }
+      void hit() {
+        health--;
+      }
+      void teleport(int x, int y) {
+        x_coordinate = x;
+        y_coordinate = y;
+      }
+      bool collision_detection(Alien a) {
+        return a.x_coordinate == x_coordinate && a.y_coordinate == y_coordinate;
+      }
+
+
+    private:
+      int health{3};
+  };
 
 }  // namespace targets
+
+int main() {
+  return 0;
+}
